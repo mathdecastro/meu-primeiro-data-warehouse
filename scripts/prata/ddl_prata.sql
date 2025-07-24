734c6c0ff9da1d6d51178b9e0ad377e7ca77c5b9
mathdecastro <theusrjxd@gmail.com>
2025-07-24 18:20:59 -0300

    :tada: feat: adicionando coluna técnica de data de criação do dado

--- scripts/prata/ddl_prata.sql
+++ scripts/prata/ddl_prata.sql
@@ -24,7 +24,8 @@ CREATE TABLE prata.crm_clientes_info(
     clientes_ultimo_nome VARCHAR(50),
     clientes_estado_civil VARCHAR(50),
     clientes_sexo VARCHAR(50),
-    clientes_data_cadastro DATE
+    clientes_data_cadastro DATE,
+    dwh_data_criacao TIMESTAMP DEFAULT CURRENT_DATE
 );
 
 -- Criando tabela prata.crm_produtos_info utilizando a bronze.crm_prd_info
@@ -36,7 +37,8 @@ CREATE TABLE prata.crm_produtos_info(
     produtos_custo INT,
     produtos_linha_producao VARCHAR(50),
     produtos_data_inicio DATE,
-    produtos_data_final DATE
+    produtos_data_final DATE,
+    dwh_data_criacao TIMESTAMP DEFAULT CURRENT_DATE
 );
 
 -- Criando tabela prata.crm_vendas_detalhes utilizando a bronze.crm_sales_details
@@ -50,7 +52,8 @@ CREATE TABLE prata.crm_vendas_detalhes(
     vendas_data_vencimento VARCHAR(50),
     vendas_valor_venda INT,
     vendas_quantidade INT,
-    vendas_preco INT
+    vendas_preco INT,
+    dwh_data_criacao TIMESTAMP DEFAULT CURRENT_DATE
 );
 
 -- Criando tabela prata.erp_clientes_info utilizando a bronze.erp_cust_az12
@@ -58,14 +61,16 @@ DROP TABLE IF EXISTS prata.erp_clientes_info;
 CREATE TABLE prata.erp_clientes_info(
     clientes_id VARCHAR(50),
     clientes_data_nascimento DATE,
-    clientes_sexo VARCHAR(50)
+    clientes_sexo VARCHAR(50),
+    dwh_data_criacao TIMESTAMP DEFAULT CURRENT_DATE
 );
 
 -- Criando tabela prata.erp_clientes_pais utilizando a bronze.erp_loc_a101
 DROP TABLE IF EXISTS prata.erp_clientes_pais;
 CREATE TABLE prata.erp_clientes_pais(
     clientes_id VARCHAR(50),
-    clientes_pais VARCHAR(50)
+    clientes_pais VARCHAR(50),
+    dwh_data_criacao TIMESTAMP DEFAULT CURRENT_DATE
 );
 
 -- Criando tabela prata.erp_produtos_cat utilizando a bronze.erp_px_cat_g1v2
@@ -74,5 +79,6 @@ CREATE TABLE prata.erp_produtos_cat(
     produtos_id VARCHAR(50),
     produtos_categoria_produto VARCHAR(50),
     produtos_subcategoria_produto VARCHAR(50),
-    produtos_manutencao VARCHAR(50)
+    produtos_manutencao VARCHAR(50),
+    dwh_data_criacao TIMESTAMP DEFAULT CURRENT_DATE
 );
